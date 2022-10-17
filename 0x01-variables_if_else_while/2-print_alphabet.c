7831b7051efa0c16c7fa8230cbf6bc30863b3787
mudesignz <ardismu@outlook.com>
2022-10-17 01:29:55 +0000

    third

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'The last digit'
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char tolower[x];
+
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	return 0;
+}
