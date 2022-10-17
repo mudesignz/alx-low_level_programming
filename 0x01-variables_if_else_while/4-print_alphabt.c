3285f804ee4647c59fd644a4c2c7f98e620877f2
mudesignz <ardismu@outlook.com>
2022-10-17 02:45:45 +0000

    commits

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'The last digit'
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch = 'e' || ch = 'q')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
