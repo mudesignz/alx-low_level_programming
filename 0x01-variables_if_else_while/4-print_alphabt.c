5e91bd700ec86ad7a6d54d6478a6e4e39cea95b0
mudesignz <ardismu@outlook.com>
2022-10-17 02:51:37 +0000

    commits

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,11 +15,11 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch = 'e' || ch = 'q')
+		putchar(ch);
+		if ((ch = 'e') && (ch = 'q'))
 		{
 			continue;
 		}
-		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
