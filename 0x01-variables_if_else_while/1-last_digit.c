6ef0c83fe66fd3b4796c9bfc6391270ef58238df
mudesignz <ardismu@outlook.com>
2022-10-17 00:14:22 +0000

    Second commits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	{
 		printf("Last digit of %d is greater than 0 ", n);
 	}
-	else if (n % 10 < 6 && != 0)
+	else if (n % 10 < 6 && n % 10 != 0)
 	{
 		printf("Last digit of %d is less than 6 and not 0", n);
 	}
