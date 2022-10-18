34cccada2f87595174f59a108ac503771815ad75
mudesignz <ardismu@outlook.com>
2022-10-18 15:51:38 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main()
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str1);
-	return 0;
+	return (0);
 }
