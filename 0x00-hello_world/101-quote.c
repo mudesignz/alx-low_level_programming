95dab97e8883dd670212402c103ca68113c994ab
mudesignz <ardismu@outlook.com>
2022-10-18 20:20:26 +0000

    Last changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_text(void)
+{
+	char str1[t] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+}
+/**
+ * main starts here
+ *
+ * Retrun should be 0
+*/
+
+int main(void)
+{
+	int t;
+	char ch;
+
+	for (*ch != '\0' ch++)
+		putchar(*ch);
+	return (1);
+}
