145eb4e7ff240e9434138d761339974eecff2ebb
mudesignz <ardismu@outlook.com>
2022-10-18 16:01:59 +0000

    Hello, Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	puts(str1 "\n");
+	return (0);
+}
