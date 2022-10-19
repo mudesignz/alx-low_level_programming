b15e60952b1728f847d2b784d4b0d6df33bc7911
mudesignz <ardismu@outlook.com>
2022-10-19 02:49:51 +0000

    changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
-	int i, x;
+	int i;
+	int x = 0;
 
-	while (x <= 10)
-			{
-			for (i = 'a'; i <= 'z'; i++)
-			{
+	while (x < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
 			_putchar(i);
-			}
-			_putchar('\n');
-			x++;
-			}
+		}
+		_putchar('\n');
+		x++;
+	}
 }
