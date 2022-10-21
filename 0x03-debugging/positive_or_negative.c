07f6f3b4473bf581694ffede901ddf2b7d0000ac
mudesignz <ardismu@outlook.com>
2022-10-21 23:39:48 +0000

    0 Postive anything

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+void positive_or_negative(int i)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
+}
