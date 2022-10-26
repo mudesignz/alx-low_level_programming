a09d19aec684cb1601f0d2cfbd53f141aaf33f6e
mudesignz <ardismu@outlook.com>
2022-10-26 16:47:00 +0000

    Last one

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,16 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int ch, a, b;
+	int ch, a, b, c;
 
 	ch = 0;
-	for (ch = 1; ch < 90; ch++)
+	for (ch = 1; ch < 999; ch++)
 	{
 		a = ch / 100;
 		b = ch / 10;
 		c = ch % 10;
 
-		if (a > b && b > c || b == a)
+		if (((a > b) || (b > c) || (a > c)) || ((a == b) || (a > c) || (b == c)))
 		{
 			continue;
 		}
