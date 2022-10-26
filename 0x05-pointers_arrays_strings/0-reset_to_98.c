2cdccc571f66c08e411a56cdf4f76d54b7a370e3
mudesignz <ardismu@outlook.com>
2022-10-26 21:10:46 +0000

    First Commit, assignment 5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - check the code 
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(void)
+{
+	int i;
+	int* p = &i;
+	*p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_   /* Include guard */
+#define MAIN_H_
+
+int foo(int x);  /* An example function declaration */
+void reset_to_98(int *n);
+
+#endif // MAIN_H_
