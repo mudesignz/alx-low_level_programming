a743507a71e4be0ef8f393419bbfde266d8b7160
mudesignz <ardismu@outlook.com>
2022-10-26 22:56:03 +0000

    all commits

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - entry point
+ * @a: data input by user
+ * @b: data input by user
+ * Description: 'returns int to main program'
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
