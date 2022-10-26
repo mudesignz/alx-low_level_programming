522c3f0d78a0f20bb1751b5c914cc6e7f43f9d1d
mudesignz <ardismu@outlook.com>
2022-10-26 09:17:32 +0000

    changes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'The last digit'
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int ch;
+
+	ch = 0;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
