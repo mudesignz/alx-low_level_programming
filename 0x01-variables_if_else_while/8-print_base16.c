5550be0695c9aa9d1c12c25bb085d72cae457670
mudesignz <ardismu@outlook.com>
2022-10-26 08:31:31 +0000

    messages

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,13 @@
 int main(void)
 {
 	int ch;
+	char hex[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 	ch = 0;
 
-	for (ch = 0; ch <= 16; ch++)
+	for (ch = 0; ch <= 15; ch++)
 	{
-		putchar((ch % 16) + '0');
+		putchar(hex[ch]);
 	}
 	putchar('\n');
 	return (0);
