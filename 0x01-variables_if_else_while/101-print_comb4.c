829501712b8a7adcb37f7a1d0cf7f5bc74d723f5
mudesignz <ardismu@outlook.com>
2022-10-26 16:08:33 +0000

    Greatest hack ever

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,17 +13,13 @@ int main(void)
 	int ch, a, b;
 
 	ch = 0;
-	for (ch = 1; ch < 90; ch++)
+	for (ch = 0; ch < 90; ch++)
 	{
 		a = ch / 10;
 		b = ch % 10;
-		if (a > b && b < a)
-		{
-			continue;
-		}
 
-		putchar((a) + '0');
-		putchar((b) + '0');
+		putchar((ch % 10) + '0');
+		putchar((ch / 10) + '0');
 		if (ch == 89)
 		{
 			break;
