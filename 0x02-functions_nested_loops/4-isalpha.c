45062ebbbbfe7ed6438a6a40b09c6b5e28625006
mudesignz <ardismu@outlook.com>
2022-10-28 00:04:21 +0300

    Changes

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - check the code.
+ * @c: is a single letter
+ * Return: 1 if int c is lowercase, else 0
+ */
+int _isalpha(int c)
+{
+        if (c >= 'A' && c <= 'z')
+        {
+                return (1);
+        }
+        else
+                return (0);
+}
