e5b41f0c68969d68c5e7789ff3f00be6d4e08573
mudesignz <ardismu@outlook.com>
2022-10-27 21:51:24 -0700

    commits

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i, x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (i = 0 && i < 14)
+		{
+			_putchar((i) + '0');
+		}
+	}
+	_putchar('\n');
+}
