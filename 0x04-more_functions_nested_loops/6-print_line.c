dae2e3b8d2782e596cab9638849697e68cc6f327
mudesignz <ardismu@outlook.com>
2022-10-27 23:35:39 -0700

    changes

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - check the code
+ * @n: input number of dashes
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
