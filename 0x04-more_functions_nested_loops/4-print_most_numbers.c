374b9f397e816fc6a02bae05e957860561645554
mudesignz <ardismu@outlook.com>
2022-10-27 21:44:30 -0700

    changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,15 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
-
-	while (i < 10)
+	int i;
+	
+	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i ==4)
 		{
 			continue;
 		}
 		_putchar((i) + '0');
-		i++;
 	}
 	_putchar('\n');
 }
