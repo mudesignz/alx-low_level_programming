816d84ae400c3bff1901f9e20afe655a256dfa34
mudesignz <ardismu@outlook.com>
2022-10-27 20:34:15 -0700

    changes

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper- check the code.
- * @c: character input
- * Return: Always 1 if c is upper case. otherwise 0..
+ * mul - check the code.
+ * @a: character input
+ * @b: character input
+ * Return: Always muliples . otherwise 0..
 */
 int mul(int a, int b)
 {
 	int mul;
-	
+
 	mul = a * b;
 	return (mul);
 }
