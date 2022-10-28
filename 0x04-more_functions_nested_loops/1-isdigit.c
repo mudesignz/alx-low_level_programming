0a807be2adde1853fd3152b6a66a6da952b5177b
mudesignz <ardismu@outlook.com>
2022-10-27 19:26:30 -0700

    commits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit- check the code.
+ * @c: character input
+ * Return: Always 1 if c is digit. otherwise 0..
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
