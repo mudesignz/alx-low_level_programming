c57c94cc6d6e4c31b16c7d1b8593fbb62920b7e4
mudesignz <ardismu@outlook.com>
2022-12-14 18:46:25 -0800

    commits

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - check the code.
+ * @n: is a single letter
+ * Return: 1 if is positive number, else 0
+ */
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+		n = n*1
+		_putchar(n + 0);
+	}
+	else
+	{
+		n = n*-1;
+		_putchar(n + 0);
+	}
+	return (0);
+}
